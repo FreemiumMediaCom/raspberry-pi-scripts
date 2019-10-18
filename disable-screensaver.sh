a515972cf668d1d28ab8dadb863407a4016023db
freemium media <sean@freemiummedia.com>
2019-10-17 22:35:20 -0400

    Adding new line

--- disable-screensaver.sh
+++ disable-screensaver.sh
@@ -1,3 +1,4 @@
+echo '' >> ~/.config/lxsession/LXDE-pi/autostart
 echo '@xset s off' >> ~/.config/lxsession/LXDE-pi/autostart
 echo '@xset -dpms' >> ~/.config/lxsession/LXDE-pi/autostart 
 echo '@xset s noblank' >> ~/.config/lxsession/LXDE-pi/autostart
