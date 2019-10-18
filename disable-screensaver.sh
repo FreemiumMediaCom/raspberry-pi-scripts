b65438bcea008914daa8f47bb693394239fc48a7
freemium media <sean@freemiummedia.com>
2019-10-17 21:51:53 -0400

    files

---
+++ disable-screensaver.sh
@@ -0,0 +1,5 @@
+echo '@xset s off' >> ~/.config/lxsession/LXDE-pi/autostart
+echo '@xset -dpms' >> ~/.config/lxsession/LXDE-pi/autostart 
+echo '@xset s noblank' >> ~/.config/lxsession/LXDE-pi/autostart
+echo '@unclutter' >> ~/.config/lxsession/LXDE-pi/autostart
+
